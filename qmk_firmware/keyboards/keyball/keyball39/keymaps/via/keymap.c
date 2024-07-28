f9a66e265591639c9eea62d0e95e4441c24be9bc
Polaris-ch <s.github@assz.me>
2024-07-28 15:53:42 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/keymap.c
@@ -85,7 +85,7 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             rgblight_sethsv(HSV_GREEN);
             break;
         case 3:
-            rgblight_sethsv(HSV_WHITE);
+            rgblight_sethsv(HSV_RED);
             break;
     }
 
