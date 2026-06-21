92b94e4e83c98a482a6610d43dc675cff638e554
Polaris-ch <s.github@assz.me>
2026-06-21 15:48:49 +0900

    Update config.h

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -40,3 +40,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define DYNAMIC_KEYMAP_LAYER_COUNT 7
 #define TAPPING_TERM 200 // ミリ秒
 #define KEYBALL_KEEP_MAGIC_FUNCTIONS
+#define NO_ACTION_ONESHOT
